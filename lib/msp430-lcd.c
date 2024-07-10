dfd40441c4ecbb17c395249e5d867b157796b507
00lucasm <lucs.r06@gmail.com>
2024-07-09 23:16:17 -0300

    add MSP430 lcd modules

--- lib/msp430-lcd.c
+++ lib/msp430-lcd.c
@@ -1,13 +1,16 @@
 #include <msp430.h>
+
 #include "msp430-lcd.h"
 
 void lcd_init(void){
 
-    UCB0I2CSA = PCF_ADR;
+    UCB0I2CSA = PCF_ADDR;
     UCB0CTL1 |= UCTR | UCTXSTT;
 
     while((UCB0IFG & UCTXIFG) == 0);
+
     UCB0TXBUF = 0;
+
     while((UCB0CTL1 & UCTXSTT) == UCTXSTT);
 
     if((UCB0IFG & UCNACKIFG) == UCNACKIFG){
@@ -24,21 +27,174 @@ void lcd_init(void){
     delay(10000);
     lcd_aux(2);
 
-    lcd_aux(2);lcd_aux(8);
+    lcd_aux(2);     lcd_aux(8);
+    lcd_aux(0);     lcd_aux(8);
+    lcd_aux(0);     lcd_aux(1);
+    lcd_aux(0);     lcd_aux(6);
+    lcd_aux(0);     lcd_aux(0xF);
+
+    while((UC0IFG & UCTXIFG) == 0);
+
+    UCB0CTL1 |= UCTXSTP;
+
+    while((UCB0CTL0 & UCTXSTP) == UCTXSTP);
+
+    delay(50);
 
 }
 
-void lcd_aux(char data);
-int pcf_read(void);
-void pcf_write(char data);
-int pcf_teste(char addr);
+void lcd_aux(char data){
+
+    while((UCB0IFG & UCTXIFG) == 0);
+
+    UCB0TXBUF = ((data << 4) & 0xF0) | BIT3;
+
+    delay(50);
+
+    while((UCB0IFG & UCTXIFG) == 0);
+
+    UCB0TXBUF = ((data << 4) & 0xF0) | BIT3 | BIT2;
+
+    delay(50);
+
+    while((UCB0IFG & UCTXIFG) == 0);
+
+    UCB0TXBUF = ((data << 4) & 0xF) | BIT3;
+
+}
+
+int pcf_read(void){
+
+    int data;
+
+    UCB0I2CSA = PCF_ADDR;
+    UCB0CTL1 &= ~UCTR;
+    UCB0CTL1 |= UCTXSTT;
+
+    while((UCB0CTL1 & UCTXSTT) == UCTXSTT);
+
+    UCB0CTL1 |= UCTXSTP;
+
+    while((UCB0CTL1 & UCTXSTP) == UCTXSTP);
+
+    while((UCB0IFG & UCRXIFG) == 0);
+
+    data = UCB0RXBUF;
+
+    return data;
+
+}
+
+void pcf_write(char data){
+
+    UCB0I2CSA = PCF_ADDR;
+    UCB0CTL1 |= UCTR | UCTXSTT;
+
+    while((UCB0IFG & UCTXIFG) == 0);
+
+    UCB0TXBUF = data;
+
+    while((UCB0CTL1 & UCTXSTT) == 0);
+
+    if((UCB0IFG & UCNACKIFG) == UCNACKIFG){
+        while(TRUE);
+    }
+
+    UCB0CTL1 |= UCTXSTP;
+
+    while((UCB0CTL1 & UCTXSTP) == UCTXSTP);
+
+}
+
+int pcf_test(char addr){
+
+    UCB0I2CSA = addr;
+    UCB0CTL1 |= UCTR | UCTXSTT;
+
+    while((UCB0IFG & UCTXIFG) == 0);
+
+    UCB0CTL1 |= UCTXSTP;
+
+    while((UCB0CTL1 & UCTXSTP) == UCTXSTP);
+
+    if((UCB0IFG & UCNACKIFG) == 0)
+        return TRUE;
+    else
+        return FALSE;
+
+}
+
+void led_i2c_config(void){
+
+    UCB0CTL1 |= UCSWRST;
+
+    UCB0CTL0 = UCSYNC | UCMODE_3 | UCMST;
+
+    UCB0BRW = BR_100K;
+
+    P3SEL |= BIT1 | BIT0;
+
+    UCB0CTL1 = UCSSEL_2;
+
+}
+
 void led_vd(void);
 void led_VD(void);
 void led_vm(void);
 void led_VM(void);
-void i2c_config(void);
 void gpio_config(void);
-void delay(long limite);
-void lcd_char(char x);
-void lcd_cursor(char x);
-void lcd_str(char *ptr);
+
+void delay(long limit){
+
+    volatile long count = 0;
+
+    while(count++ < limit);
+
+}
+
+void lcd_char(char x){
+
+    char lsn, msn;
+
+    msn = (x & 0xF0);
+    lsn = ((x & 0xF) << 4);
+
+    pcf_write(msn | 9);
+    pcf_write(msn | 0xD);
+    pcf_write(msn | 9);
+    pcf_write(lsn | 9);
+    pcf_write(lsn | 0xD);
+    pcf_write(lsn | 9);
+
+}
+
+void lcd_cursor(char x){
+
+    char lsn, msn;
+    x |= 0x80;
+
+    msn = (x & 0xF0);
+    lsn = ((x & 0xF) << 4);
+
+    pcf_write(msn | 8);
+    pcf_write(msn | 0xC);
+    pcf_write(msn | 8);
+    pcf_write(lsn | 8);
+    pcf_write(lsn | 0xC);
+    pcf_write(lsn | 8);
+
+}
+
+void lcd_str(char *ptr){
+
+    char i = 0;
+
+    while(ptr[i] != '\0'){
+        if((15 - i) > 0){
+            lcd_char(ptr[i]);
+        }else{
+            lcd_cursos(LINE_1_INIT);
+        }
+    }
+
+}
--- lib/msp430-lcd.h
+++ lib/msp430-lcd.h
@@ -1,19 +1,20 @@
 #include <msp430.h>
 
-#ifndef _MSP430-LCD_H_
-#define _MSP430-LCD_H_
+#ifndef MSP430_LCD_H_
+#define MSP430_LCD_H_
 
 #define TRUE    1
 #define FALSE   0
+
 #define LINE_0_INIT 0
 #define LINE_0_END  0xF
 #define LINE_1_INIT 0x40
 #define LINE_1_END  0x4F
 
-// Definicao do endereco do PCF_8574
-#define PCF_ADR1 0x3F
-#define PCF_ADR2 0x27
-#define PCF_ADR  PCF_ADR1
+// PCF_8574 address
+#define PCF_ADDR1 0x3F
+#define PCF_ADDR2 0x27
+#define PCF_ADDR  PCF_ADDR1
 
 #define BR_100K    11  // SMCLK/100K = 11
 #define BR_50K     21  // SMCLK/50K  = 21
@@ -22,15 +23,15 @@
 void lcd_init(void);
 void lcd_aux(char data);
 int pcf_read(void);
-void pcf_write(char dado);
-int pcf_teste(char adr);
+void pcf_write(char data);
+int pcf_test(char addr);
 void led_vd(void);
 void led_VD(void);
 void led_vm(void);
 void led_VM(void);
-void i2c_config(void);
-void gpio_config(void);
-void delay(long limite);
+void lcd_i2c_config(void);
+void lcd_gpio_config(void);
+void delay(long limit);
 void lcd_char(char x);
 void lcd_cursor(char x);
 void lcd_str(char *ptr);
