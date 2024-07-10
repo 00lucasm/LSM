848adafe5e9b593251148b3ada69fe9ee6f9a695
00lucasm <lucs.r06@gmail.com>
2024-07-09 23:15:25 -0300

    fixing whitespaces issues

--- lib/msp430-oled.c
+++ lib/msp430-oled.c
@@ -1,4 +1,5 @@
 #include <msp430.h>
+
 #include "msp430-oled.h"
 #include "font5x7.h"
 
@@ -32,7 +33,9 @@ void i2c_write_byte(unsigned char slave_addr, unsigned char reg_addr, unsigned c
 }
 
 void oled_write_command(unsigned char command){
+
     i2c_write_byte(OLED_I2C_ADDR, 0x00, command);
+    
 }
 
 void oled_init(void){
